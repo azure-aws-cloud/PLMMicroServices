66d756e49a2469ab4ed8f4ecf7711dea915b552c
Rhushikesh Velankar <51327670+rhushikeshv@users.noreply.github.com>
2024-10-22 07:49:13 +0530

    db credentials configured

--- database.py
+++ database.py
@@ -1,9 +1,32 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # SQLALCHEMY_DATABASE_URL = "postgresql://postgres:123@host.docker.internal/postgres"
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:123@localhost/postgres"
+
+db_user = os.getenv('DB_USERNAME')
+if db_user is None:
+    db_user = 'postgres'
+
+db_pass = os.getenv('DB_PASSWORD')
+if db_pass is None:
+    db_pass = 123
+
+db_host = os.getenv('DB_HOST')
+if db_host is None:
+    db_host = 'localhost'
+
+db_port = os.getenv('DB_PORT')
+if db_port is None:
+    db_port = 5432
+
+db_schema = os.getenv('DB_SCHEMA')
+if db_schema is None:
+    db_schema = 'plm'
+
+SQLALCHEMY_DATABASE_URL = f"postgresql://{db_user}:{db_pass}@{db_host}/{db_schema}"
   # Use your database URL here
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
